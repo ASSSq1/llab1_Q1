dbf5c36b95f2dcc886670fbd230b726ce98b1829
ASSSq1 <113017117+ASSSq1@users.noreply.github.com>
2022-10-08 23:07:21 +0300

    Add files via upload

---
+++ lab1-Q1/Untitled1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+void trig_func(double a,double s,double d)
+int main() {
+    double a;
+    int *p=&a;
+cout << "Type a number: ";
+ << sin(a)*180/M_PI << endl;
+cin >> a;
+trig_func(a);
+ cout<<p<<;
+double s;
+int *p=&s;
+cout << "Type a number: ";
+ << cos(s)*180/M_PI << endl;
+cin >> s;
+trig_func(s)
+ cout<<p<<;
+double d;
+int *p=&d;
+cout << "Type a number: ";
+ << tan(d)*180/M_PI << endl;
+cin >> d;
+trig_func(d)
+ cout<<p<<;
+
+    return 0;
+}
